5ce49db828f9b14679e99232ef86a2d1e6879d75
DESKTOP-45DBCCQ\pc <1>
2021-06-08 20:31:31 +0900

    3rd commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 let router = require('./router/main')(app);
-let database = require("./database/config");
+//let database = require("./database/config"); 
 let port = process.env.PORT || 3000;
 
 app.set('views', __dirname + '/views');
--- router/main.js
+++ router/main.js
@@ -1,6 +1,9 @@
 const bodyParser = require('body-parser');
-var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/test";
+//var MongoClient = require("mongodb").MongoClient;
+//var url = "mongodb://localhost:27017/test";
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const alert = require('alert');
 
 module.exports = function(app) {
      app.use(bodyParser.json());       // to support JSON-encoded bodies
@@ -74,7 +77,7 @@ module.exports = function(app) {
           var dbo = db.db("mydb");          
           dbo
             .collection("accounthistory")
-            .find({})
+            .find({acntitem : req.body.shAcntItem})
             .toArray(function(err, result) {
               resultList = result;
               if (err) throw err;
@@ -370,5 +373,163 @@ module.exports = function(app) {
     );
   });
 
+  app.post("/sendPwdEmail", function(req, res) {
+    //res.status(400).send('email required');
+    const token = crypto.randomBytes(5).toString('hex'); // token 생성  20
+    const data = { // 데이터 정리
+      token,
+      //userId: 'asdf',//user.id
+      ttl: 6000 // ttl 값 설정 (100분)
+    }; 
 
+    // updated
+    MongoClient.connect(
+      "mongodb://localhost:27017",
+      { useUnifiedTopology: true },
+      function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        //req.body.putAcntAmount
+        var myquery = { id: req.body.findPwdId, roll: req.body.findPwdRoll };
+        var newvalues = { $set: { pwd: token } };
+        dbo
+          .collection("privacy")
+          .updateOne(myquery, newvalues, function(err, res) {
+            if (err) {alert("일치하는 정보를 찾을 수 없습니다.");
+               throw err;
+            }else{
+              //메일 전송
+              const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true, // true for 465, false for other ports
+                auth: { // 이메일을 보낼 계정 데이터 입력
+                  user: 'mejjh0416@gmail.com',
+                  pass: '',
+                },
+              });
+              const emailOptions = { // 옵션값 설정
+                  from: 'mejjh0416@gmail.com',
+                  to: 'mejjh0416@naver.com',
+                  subject: '비밀번호 초기화 이메일입니다.',
+                  text: 'This is the authentication code to find the password!', // 내용
+                  html: '비밀번호 초기화를 위한 임시 비밀번호 입니다. '
+                    + `임시비밀번호 : ${token}`
+                  //+ `http://localhost/resetpwd/${token}`
+                   ,
+              };  
+          
+              transporter.sendMail(emailOptions, function(error, res){
+          
+                if (error){
+                  console.log(error,"error T.T");
+                } else {
+                  console.log("Message sent : " + res.message);
+                }
+                transporter.close();
+              });
+
+            }
+            console.log("1 document updated");
+            db.close();
+          });
+      }
+    );
+
+    
+    res.render('login.html')
+  });
+
+  app.post("/insertPrivacy", function(req, res) {
+    // res.render("table.html");
+    let resultList;
+ 
+        // Insert data
+    MongoClient.connect(
+      "mongodb://localhost:27017",
+      { useUnifiedTopology: true },
+      function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        let postsignEmail = req.body.signEmail;
+        let postPutAcntItem = req.body.putAcntItem;
+        let postPutAcntAmount = req.body.putAcntAmount;
+        let postPutAcntEtc = req.body.putAcntEtc;
+        let myobj = { acntid: "asdf", acntdate: postPutAcntDate, acntitem: postPutAcntItem , acntamount: postPutAcntAmount , acntetc: postPutAcntEtc };
+        dbo.collection("accounthistory").insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted");
+          db.close();
+        });
+      }
+    );
+    res.render('index.html')
+  });
+
+  app.post("/loginIn", function(req, res) {
+    // res.render("table.html");
+    let resultList;
+ 
+    // // Find name and address
+    MongoClient.connect(
+      "mongodb://localhost:27017",
+      { useUnifiedTopology: true },
+      function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("privacy").findOne({id: req.body.loginId, pwd: req.body.loginPwd}, function(err, result) {
+            resultList = result;
+            if (err) throw err;
+
+            if(resultList) res.render("index.html", { userData_list: resultList });
+            if(!resultList) alert("정보가 일치하지 않습니다.");
+
+            console.log("input loginId and loginPwd : ", req.body.loginId," & ",req.body.loginPwd);
+            console.log("find Id and Pwd : ", result);
+            
+            db.close();
+          });
+      }
+    ); 
+
+    // Join the matching "products" document(s) to the "orders" collection:
+    MongoClient.connect(
+      "mongodb://localhost:27017",
+      { useUnifiedTopology: true },
+      function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo
+          .collection("privacy")
+          .aggregate([
+            {
+              $lookup: {
+                from: "goal",
+                localField: "id",
+                foreignField: "gid",
+                as: "id"
+              }
+            }
+          ])
+          .toArray(function(err, res) {
+            resultList = result;
+            if (err) throw err;
+            if (resultList) res.render("index.html", { userData_list: resultList });
+            if (!resultList) alert("정보가 일치하지 않습니다.");
+
+            console.log("input loginId and loginPwd : ", req.body.loginId," & ",req.body.loginPwd);
+            console.log("find Id and Pwd : ", result);            
+            console.log("aggregate: ", JSON.stringify(res));
+            db.close();
+          });
+      }
+    );
+
+  });
 };
+
+
+
+
+
